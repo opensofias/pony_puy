7b8e15004790945418f6e7456e2bc05ba2c79f92
sofia <12581975+opensofias@users.noreply.github.com>
2023-03-13 01:34:01 +0100

    hyperIter function

--- tools.js
+++ tools.js
@@ -5,12 +5,32 @@ export const elem = ({tag = 'div', attr = {}, content = [], svg = false}) => {
 
 	for (const name in attr) result.setAttribute(name, attr[name])
 
-	void (type => 
+	void (type =>
 		['string', 'number'].includes (type) ?
 			result.innerText = content :
 		type == 'object' && [content].flat ()
 			.forEach(contEl => result.appendChild(contEl))
 	) (typeof content)
+
+	return result
+}
+
+export const hyperIter = (size = [], fun = x => x) => {
+	if (!(size.length) || Math.min (...size) < 1) return []
+	
+	const count = (new Array(size.length)).fill(0)
+	const result = []
+	let carry = 0
+	while (!carry) {
+		result.push (fun (count))
+
+		carry ++
+		size.forEach ((target, idx) => {
+			if (carry)
+				if (count[idx] == target -1) count[idx] = 0
+				else {count[idx] ++; carry -- }
+		})
+	}
 
 	return result
 }
