81b6db2d0056bdf7507d0a0ae7bd8f76b78dcdd2
sofia <12581975+opensofias@users.noreply.github.com>
2023-03-26 16:07:50 +0200

    merging elem () into ElementWrapper

--- element.js
+++ element.js
@@ -1,33 +1,28 @@
-const elem = ({
-	tag = 'div', svg = false,
-	attr = {}, style = {}, cssVar = {},
-	cls = '', id= '',
-	content = [], mixin = {},
-}) => {
-	const result = svg ?
-		document.createElementNS ('http://www.w3.org/2000/svg', tag) :
-		document.createElement (tag)
-
-	for (const name in attr) result.setAttribute(name, attr[name])
-	for (const name in style) result.style.setProperty(name, style[name])
-	for (const name in cssVar) result.style.setProperty('--' + name, cssVar[name])
-	cls && cls.split(' ').forEach (x => result.classList.add (x))
-	id && (result.id = id)
-
-	void (type =>
-		['string', 'number'].includes (type) ?
-			result.innerText = content :
-		type == 'object' && [content].flat ()
-			.forEach(contEl => result.appendChild(contEl))
-	) (typeof content)
-
-	return Object.assign (result, mixin)
-}
-
 export class ElementWrapper {
-	constructor(...param) {
-		this.element = elem(...param);
-		this.element.wrapper = this;
+	constructor({
+		tag = 'div', svg = false,
+		attr = {}, style = {}, cssVar = {},
+		cls = '', id= '',
+		content = [], mixin = {},
+	}) {
+		const result = svg ?
+			document.createElementNS ('http://www.w3.org/2000/svg', tag) :
+			document.createElement (tag)
+
+		for (const name in attr) result.setAttribute(name, attr[name])
+		for (const name in style) result.style.setProperty(name, style[name])
+		for (const name in cssVar) result.style.setProperty('--' + name, cssVar[name])
+		cls && cls.split(' ').forEach (x => result.classList.add (x))
+		id && (result.id = id)
+
+		void (type =>
+			['string', 'number'].includes (type) ?
+				result.innerText = content :
+			type == 'object' && [content].flat ()
+				.forEach(contEl => result.appendChild(contEl))
+		) (typeof content)
+
+		this.element = Object.assign (result, mixin, {wrapper: this})
 	}
 	getCssVar(prop) {
 		return this.style.getPropertyValue('--' + prop);
