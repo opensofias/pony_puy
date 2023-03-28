c9b73f2ec240a4284fa5de714b2ebd23aa1584dc
sofia <12581975+opensofias@users.noreply.github.com>
2023-03-28 12:25:22 +0200

    inserting the rules into the right sheet

--- style.js
+++ style.js
@@ -13,7 +13,7 @@ export const generateBlobStyles = () => {
 
 		const rule = `d: path('M${vec[3] ? -17 : -7},0 L-5,5 L0,${vec[2] ? 17 : 7} L5,5 L${vec[1] ? 17 : 7},0 L5,-5 L0,${vec[0] ? -17 : -7} L-5,-5 Z')`
 
-		document.styleSheets[0].insertRule(`${selector} { ${rule} }`, sheet.cssRules.length)
+		sheet.insertRule(`${selector} { ${rule} }`, sheet.cssRules.length)
 	})
 
 	document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]
