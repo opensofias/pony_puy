ba0b55d8737f025024a4391027f527ab84e4ef00
sofia <12581975+opensofias@users.noreply.github.com>
2023-04-02 15:27:44 +0200

    add "selected" CSS class when dragging

--- actions.js
+++ actions.js
@@ -4,8 +4,10 @@ export const actions = {
 	dragAndSwap : {
 		onmousedown (event) {
 			draggedFrom = this.wrapper.screen2pos (event)
+			this.wrapper.getByClass (draggedFrom).addClasses ('selected')
 		},
 		onmouseup (event) {
+			this.wrapper.getByClass (draggedFrom).removeClasses ('selected')
 			this.wrapper.swap (draggedFrom, this.wrapper.screen2pos (event))
 		}
 	}
