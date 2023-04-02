9818d2d3820b909ae1d362db4a440aa2fac7467f
sofia <12581975+opensofias@users.noreply.github.com>
2023-04-02 02:48:02 +0200

    experimenting with basic drag&drop support

---
+++ actions.js
@@ -0,0 +1,6 @@
+export const actions = {
+	dragAndSwap : {
+		onmousedown (...params) {console.log (...params)},
+		onmouseup (...params) {console.log (...params)}
+	}
+}
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { generateBlobStyles } from "./style.js"
 import { Playfield } from "./playfield.js";
+import { actions } from "./actions.js";
 
 const playfield = new Playfield ();
 window.playfield = playfield
@@ -7,3 +8,6 @@ document.body.appendChild(playfield.element);
 playfield.fill()
 playfield.blobAll()
 generateBlobStyles ()
+
+// temporary colution for building drag&drop
+Object.assign (playfield.element, actions.dragAndSwap)
