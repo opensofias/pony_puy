ef9ecc64cc79da0ee9c1abce1f8c3bd5eb8026ba
sofia <12581975+opensofias@users.noreply.github.com>
2023-04-02 04:34:48 +0200

    accessing playfield though "this" is a bit nicer.

--- actions.js
+++ actions.js
@@ -3,10 +3,10 @@ let draggedFrom
 export const actions = {
 	dragAndSwap : {
 		onmousedown (event) {
-			draggedFrom = playfield.screen2pos (event)
+			draggedFrom = this.wrapper.screen2pos (event)
 		},
 		onmouseup (event) {
-			playfield.swap (draggedFrom, playfield.screen2pos (event))
+			this.wrapper.swap (draggedFrom, this.wrapper.screen2pos (event))
 		}
 	}
 }
