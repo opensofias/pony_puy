fb1ca7d9b9a2b8e1779f953d48752068b896b103
sofia <12581975+opensofias@users.noreply.github.com>
2023-04-24 00:49:54 +0200

    bodyEvents and ther registration

--- actions.js
+++ actions.js
@@ -15,15 +15,26 @@ const actions = {
 				this.wrapper.swap (draggedFrom, this.wrapper.screen2pos (event))
 			}
 		},
-		dragged: new Map ()
+		bodyEvents: {
+			pointerleave (event) {
+				const draggedFrom = actions.dragAndSwap.dragged.get (event.pointerId)
+				actions.dragAndSwap.dragged.delete (event.pointerId)
+			}
+		},
+		dragged: new Map (),
 	}
 }
 
 export const registerActions = (field, actionsName) => {
-	if (currentActionScheme)
+	if (currentActionScheme) {
 		for (const type in actions [currentActionScheme].fieldEvents)
-		field.removeEventListener (type, actions [currentActionScheme].fieldEvents [type])
+			field.removeEventListener (type, actions [currentActionScheme].fieldEvents [type])
+		for (const type in actions [currentActionScheme].bodyEvents)
+			document.body.removeEventListener (type, actions [currentActionScheme].bodyEvents [type])
+	}
 	for (const type in actions [actionsName].fieldEvents)
 		field.addEventListener (type, actions [actionsName].fieldEvents [type])
+	for (const type in actions [actionsName].bodyEvents)
+		document.body.addEventListener (type, actions [actionsName].bodyEvents [type])
 	currentActionScheme = actionsName
 }
