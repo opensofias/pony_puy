80bf7920ab5e49587b3ec9221ce7cc678bd6854b
sofia <12581975+opensofias@users.noreply.github.com>
2023-05-07 05:47:18 +0200

    countRows function

--- matching.js
+++ matching.js
@@ -1,1 +1,17 @@
-// TODO
+import { hyperIter } from "./tools"
+
+const countRows = (field, {dx = 0, dy = 0}) => {
+	const result = new Set ()
+	hyperIter ([field.high, field.wide], ([y, x]) => {
+		const current = field.getByClass ({x, y})
+		const match = field.getByClass ({
+			y: y + dy, x: x + dx, color: current.color
+		})
+		if (match)
+			result.forEach (group => group.has (match) && group.add (current))
+		else
+			result.add (new Set ([current]))
+	})
+	return result
+}
+
