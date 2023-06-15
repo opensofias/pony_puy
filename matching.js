e5f31a12d40ac277c2b8792bbfb4edebc2b68f24
sofia <12581975+opensofias@users.noreply.github.com>
2023-06-15 03:46:18 +0200

    fixing countRows

--- matching.js
+++ matching.js
@@ -4,13 +4,18 @@ const countRows = (field, {dx = 0, dy = 0}) => {
 	const result = new Set ()
 	hyperIter ([field.high, field.wide], ([y, x]) => {
 		const current = field.getByClass ({x, y})
+		let currentGroup
+		result.forEach (group => {group.has (current) && (currentGroup = group)})
+		if (!currentGroup) {
+			currentGroup = new Set ([current])
+			result.add (currentGroup)
+		}
+
 		const match = field.getByClass ({
 			y: y + dy, x: x + dx, color: current.color
 		})
 		if (match)
-			result.forEach (group => group.has (match) && group.add (current))
-		else
-			result.add (new Set ([current]))
+			currentGroup.add (match)
 	})
 	return result
 }
