c84efd8bfb3988237ff5b338d21ec5f1a5ef603a
sofia <12581975+opensofias@users.noreply.github.com>
2023-06-15 03:50:29 +0200

    destroy3 test function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { generateBlobStyles, registerProps } from "./style.js"
 import { Playfield } from "./playfield.js";
 import { registerActions } from "./actions.js";
+import { destroyRows } from "./matching.js";
 
 const playfield = new Playfield ();
 window.playfield = playfield
@@ -13,3 +14,4 @@ registerProps ()
 registerActions (playfield, 'dragAndSwap')
 
 window.deregister = () => registerActions (playfield) // for testing if deregistering works
+window.destroy3 = () => destroyRows (playfield)
