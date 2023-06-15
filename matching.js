74c5c03a09ff52a40340b2479d6843cf9908278b
sofia <12581975+opensofias@users.noreply.github.com>
2023-06-15 04:06:36 +0200

    replacing sets with arrays

--- matching.js
+++ matching.js
@@ -1,25 +1,26 @@
 import { hyperIter } from "./tools.js"
 
 const countRows = (field, {dx = 0, dy = 0} = {dx: 1, dy: 0}) => {
-	const result = new Set ()
+	const result = []
 	hyperIter ([field.high, field.wide], ([y, x]) => {
 		const current = field.getByClass ({x, y})
 		let currentGroup
-		result.forEach (group => {group.has (current) && (currentGroup = group)})
+		result.forEach (group => {group.includes (current) && (currentGroup = group)})
 		if (!currentGroup) {
-			currentGroup = new Set ([current])
-			result.add (currentGroup)
+			currentGroup = [current]
+			result.push (currentGroup)
 		}
 
 		const match = field.getByClass ({
 			y: y + dy, x: x + dx, color: current.color
 		})
 		if (match)
-			currentGroup.add (match)
+			currentGroup.push (match)
 	})
+	console.log (result)
 	return result
 }
 
 export const destroyRows = (field, {dx = 1, dy = 0, length = 3} = {}) => {
-	countRows (field, {dx, dy}).forEach (row => (row.size >= length) && row.forEach (gem => gem.destroyGem()))
+	countRows (field, {dx, dy}).forEach (row => (row.length >= length) && row.forEach (gem => gem.destroyGem()))
 }
